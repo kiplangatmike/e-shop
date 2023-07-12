9cd1d37179b8e5b4230cf40fa8b1fd7e5c9012df
kiplangatmike <m.kiplangat@alustudent.com>
2023-07-12 23:58:01 +0200

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -19,7 +19,9 @@ export default function Login() {
         </div>
         <div>
           <button>Facebook</button>
+          
           <button>Gmail</button>
+
         </div>
       </section>
     </Main>
