833d7133b34a61015e09f30eb8835a9884709eff
kiplangatmike <m.kiplangat@alustudent.com>
2023-07-28 22:54:02 +0200

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,27 +5,22 @@ import Link from "next/link";
 export default function Login() {
   return (
     <Main>
-
       <section className="text-center">
-      
         <p className="text-red">Login</p>
         <p>Login to your account?</p>
         <div className="flex flex-col">
           <input type="text" placeholder="email" value="" />
           <input type="text" placeholder="password" value="" />
         </div>
-
         <div>
           <input type="checkbox" id="myCheckbox" />
           <label htmlFor="myCheckbox">Keep me signed in</label>
           <Link href="#">Forgot password?</Link>
         </div>
-
         <div>
           <button>Facebook</button>
           <button>Gmail</button>
         </div>
-        
       </section>
     </Main>
   );
